b42036e0bf7adaebefc41036326c3ab532f75789
Eric <dean34520@gmail.com>
2018-01-03 21:20:51 +0800

    最新版本首頁及分頁上傳
    上傳今天報告的最新版本

---
+++ css/SingUp.js
@@ -0,0 +1,7 @@
+window.onload=function singup(){
+    var lightbox=document.getElementById("lightbox");
+    var signup=document.getElementById("signup");
+    var reset=document.getElementById("reset");
+    var bottom=document.getElementById("bottom");
+
+}
