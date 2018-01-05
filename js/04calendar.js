ed26a9fe76ec8f40016224f878881d0a0257024f
Manna Chen <sbxew99@gmail.com>
2018-01-05 09:32:52 +0800

    修改月曆

--- js/04calendar.js
+++ js/04calendar.js
@@ -10,7 +10,9 @@ var iMonth = 0;
 window.addEventListener("load", iniCal);
 function iniCal(){
 	document.getElementById("showMonth").innerText = todayMonth+" "+todayYear;
-	document.getElementsByTagName("i")[0].style.visibility = "hidden";
+	document.getElementsByTagName("i")[0].style.opacity = "0.3";
+	document.getElementsByTagName("i")[0].style.cursor = "default";
+	document.getElementsByTagName("i")[0].removeEventListener("click",prevMonth);
 	for(i = 0; i < daysofMonth ; i++){
 		document.getElementsByClassName("claContent")[firstDayofMonth+i].style.opacity = "1";
 		// 小於10的日期，前面加"0"
@@ -22,26 +24,29 @@ function iniCal(){
 			document.getElementsByClassName("daysHere")[firstDayofMonth+i].innerText = i+1;
 		}
 		document.getElementsByClassName("daysHere")[firstDayofMonth+i].style.transform = "rotate3d(0,1,0,360deg)";
-		var textShadow = document.createElement("span");
-		textShadow.className = "textShadow";
-		document.getElementsByClassName("daysHere")[firstDayofMonth+i].appendChild(textShadow);
 	}
 	// 今日強調顯示
-	document.getElementsByClassName("daysHere")[today.getDate()+firstDayofMonth-1].style.backgroundColor = "#ccffff";
+	document.getElementsByClassName("daysHere")[today.getDate()+firstDayofMonth-1].style.color = "#ccffff";
+	document.getElementsByClassName("daysHere")[today.getDate()+firstDayofMonth-1].style.textShadow = "1px 1px 5px orange";
+	document.getElementsByClassName("daysHere")[today.getDate()+firstDayofMonth-1].style.backgroundColor = "#44A0D9";
 }
 
 // 回到今日：用getMonth()和getFullYear()方法，將年月資料初始化
 document.getElementById("backToToday").addEventListener("click", backToToday);
 function backToToday(){
 	iMonth = 0;
-	document.getElementsByTagName("i")[0].style.visibility = "hidden";
-	document.getElementsByTagName("i")[1].style.visibility = "visible";
+	document.getElementsByTagName("i")[0].style.opacity = "0.3";
+	document.getElementsByTagName("i")[0].style.cursor = "default";
+	document.getElementsByTagName("i")[0].addEventListener("click",prevMonth);
+	document.getElementsByTagName("i")[1].style.opacity = "1";
 	todayMonthIndex = today.getMonth();
 	todayYear = today.getFullYear();
 	document.getElementById("showMonth").innerText = todayMonth+" "+todayYear;
 	for(i = 0 ; i < 40 ; i++){
 		document.getElementsByClassName("claContent")[i].innerText = " ";
 		document.getElementsByClassName("claContent")[i].style.opacity = "0";
+		document.getElementsByClassName("daysHere")[i].style.color = "orange";
+		document.getElementsByClassName("daysHere")[i].style.textShadow = "none";
 	}
 	for(i = 0; i < daysofMonth ; i++){
 		document.getElementsByClassName("daysHere")[firstDayofMonth+i].innerText = i+1;
@@ -58,9 +63,13 @@ function prevMonth(){
 	// 更新月曆
 	iMonth--;
 	if( iMonth == 0){
-		document.getElementsByTagName("i")[0].style.visibility = "hidden";
+		document.getElementsByTagName("i")[0].style.opacity = "0.3";
+		document.getElementsByTagName("i")[0].style.cursor = "default";
+		document.getElementsByTagName("i")[0].removeEventListener("click",prevMonth);
 	}if( iMonth <= 3){
-		document.getElementsByTagName("i")[1].style.visibility = "visible";
+		document.getElementsByTagName("i")[1].style.opacity = "1";
+		document.getElementsByTagName("i")[1].style.cursor = "pointer";
+		document.getElementsByTagName("i")[1].addEventListener("click",nextMonth);
 	}
 	todayMonthIndex--;// 新月份的index
 	var newdaysofMonth = parseInt(new Date(todayYear,todayMonthIndex+1,0).toString().substr(8,2));// 新月份的天數
@@ -68,12 +77,19 @@ function prevMonth(){
 	for(i = 0 ; i < 40 ; i++){
 		document.getElementsByClassName("daysHere")[i].innerText = " ";
 		document.getElementsByClassName("daysHere")[i].style.backgroundColor = "transparent";
+		document.getElementsByClassName("daysHere")[i].style.color = "navy";
+		document.getElementsByClassName("daysHere")[i].style.textShadow = "none";
 		document.getElementsByClassName("claContent")[i].innerText = " ";
 		document.getElementsByClassName("claContent")[i].style.opacity = "0";
 	}
 	for(i = 0 ; i < newdaysofMonth ; i++){
 		document.getElementsByClassName("daysHere")[newfirstDayofMonth+i].innerText = i+1;
 		document.getElementsByClassName("claContent")[newfirstDayofMonth+i].style.opacity = "1";
+		if( iMonth == 0){
+			document.getElementsByClassName("daysHere")[today.getDate()+firstDayofMonth-1].style.color = "#ccffff";
+			document.getElementsByClassName("daysHere")[today.getDate()+firstDayofMonth-1].style.textShadow = "1px 1px 5px orange";
+			document.getElementsByClassName("daysHere")[today.getDate()+firstDayofMonth-1].style.backgroundColor = "#44A0D9";
+		}
 	}
 	// 更新#showMonth
 	document.getElementById("showMonth").innerText = new Date(todayYear,todayMonthIndex).toString().substr(4,11).replace(" 01 "," ");
@@ -86,10 +102,14 @@ function nextMonth(){
 // 更新月曆
 	iMonth++;
 	if( iMonth > 0){
-		document.getElementsByTagName("i")[0].style.visibility = "visible";
+		document.getElementsByTagName("i")[0].style.opacity = "1";
+		document.getElementsByTagName("i")[0].style.cursor = "pointer";
+		document.getElementsByTagName("i")[0].addEventListener("click",prevMonth);
 	}if( iMonth == 3){
 		// 只能查看三個月後的活動資訊
-		document.getElementsByTagName("i")[1].style.visibility = "hidden";
+		document.getElementsByTagName("i")[1].style.opacity = "0.3";
+		document.getElementsByTagName("i")[1].style.cursor = "default";
+		document.getElementsByTagName("i")[1].removeEventListener("click",nextMonth);
 	}
 	todayMonthIndex++;// 新月份的index
 	var newdaysofMonth = parseInt(new Date(todayYear,todayMonthIndex+1,0).toString().substr(8,2));// 新月份的天數
@@ -97,6 +117,8 @@ function nextMonth(){
 	for(i = 0 ; i < 40 ; i++){
 		document.getElementsByClassName("daysHere")[i].innerText = " ";
 		document.getElementsByClassName("daysHere")[i].style.backgroundColor = "transparent";
+		document.getElementsByClassName("daysHere")[i].style.color = "navy";
+		document.getElementsByClassName("daysHere")[i].style.textShadow = "none";
 		document.getElementsByClassName("claContent")[i].innerText = " ";
 		document.getElementsByClassName("claContent")[i].style.opacity = "0";
 	}
@@ -138,56 +160,151 @@ $(document).ready(function(){
 		$("#activityDate").text(showDate);
 		// 依照日期查看園區活動一覽: 星期
 		var day = ["(日)","(一)","(二)","(三)","(四)","(五)","(六)"];
-	$("#activityDay").text(day[$(this).index()/2]);
+	$("#activityDay").text(day[$(this).index()]);
 	});
 	$("#backToToday").click(function(){
 		$("#activityDate").text("今日");
 		$("#activityDay").text("");
 	});
 });
-
-// 切換活動月曆模式
+// 自動輪播月曆模式
 $(document).ready(function(){
-	$("#icon1").click(function(){
-		$(".sun").html("<img src='img/forthSection/theater.png' style='width: 25px'>");
-		$(".mon").html("");
-		$(".tue").html("<img src='img/forthSection/theater.png' style='width: 25px'>");
-		$(".wed").html("<img src='img/forthSection/theater.png' style='width: 25px'>");
-		$(".thu").html("");
-		$(".fri").html("<img src='img/forthSection/theater.png' style='width: 25px'>");
-		$(".sat").html("<img src='img/forthSection/theater.png' style='width: 25px'>");
-	});
-	$("#icon2").click(function(){
-		$(".sun").html("");
-		$(".mon").html("<img src='img/forthSection/outoforder.png' style='width: 25px'>");
-		$(".tue").html("");
-		$(".wed").html("");
-		$(".thu").html("");
-		$(".fri").html("");
-		$(".sat").html("");
-	});
-	$("#icon3").click(function(){
-		$(".sun").html("9-22");
-		$(".mon").html("休園");
-		$(".tue").html("10-22");
-		$(".wed").html("10-22");
-		$(".thu").html("10-22");
-		$(".fri").html("10-24");
-		$(".sat").html("9-24");
-	});
-	$("#icon4").click(function(){
-		$(".sun").html("<img src='img/forthSection/lecture.png' style='width: 25px'>");
-		$(".mon").html("");
-		$(".tue").html("<img src='img/forthSection/lecture.png' style='width: 25px'>");
-		$(".wed").html("");
-		$(".thu").html("<img src='img/forthSection/lecture.png' style='width: 25px'>");
-		$(".fri").html("<img src='img/forthSection/lecture.png' style='width: 25px'>");
-		$(".sat").html("<img src='img/forthSection/lecture.png' style='width: 25px'>");
-	});
-	$("#clearCal").click(function(){
-		$(".claContent").html("");
-	});
+	// 月曆預設於第一個模式
+	autoIconLoop1();
+	// 依序起跑
+	setTimeout(function(){ icon1GO()}, 0);
+	setTimeout(function(){ icon2GO()}, 5000);
+	setTimeout(function(){ icon3GO()}, 10000);
+	setTimeout(function(){ icon4GO()}, 15000);
+	// 每20秒啟動一次，並設定定時器ID
+	function icon1GO(){
+		stop1 = setInterval(function(){ autoIconLoop1(); }, 20000);
+	}
+	function icon2GO(){
+		stop2 = setInterval(function(){ autoIconLoop2(); }, 20000);
+	}
+	function icon3GO(){
+		stop3 = setInterval(function(){ autoIconLoop3(); }, 20000);	
+	}
+	function icon4GO(){
+		stop4 = setInterval(function(){ autoIconLoop4(); }, 20000);
+	}
+
+function autoIconLoop1(){
+	$(".claContent").html("");
+	$(".sun").html("尋找星生命");
+	$(".mon").html("");
+	$(".tue").html("末世決戰");
+	$(".wed").html("尋找星生命");
+	$(".thu").html("");
+	$(".fri").html("尋找星生命");
+	$(".sat").html("末世決戰");
+	$("#icon1").css("border","1px solid orange");
+	$(".icons").not("#icon1").css("border","1px solid transparent");
+};
+function autoIconLoop2(){
+	$(".claContent").html("");
+	$(".claContent:eq(5)").append("設施1");
+	$(".claContent:eq(10)").append("設施2");
+	$(".claContent:eq(15)").append("設施3");
+	$(".claContent:eq(20)").append("設施4");
+	$(".claContent:eq(25)").append("設施5");
+	$(".claContent:eq(30)").append("設施6");
+	$("#icon2").css("border","1px solid orange");
+	$(".icons").not("#icon2").css("border","1px solid transparent");
+};
+function autoIconLoop3(){
+	$(".claContent").html("");
+	$(".sun").html("9-22");
+	$(".mon").html("休園");
+	$(".tue").html("10-22");
+	$(".wed").html("10-22");
+	$(".thu").html("10-22");
+	$(".fri").html("10-24");
+	$(".sat").html("9-24");
+	$("#icon3").css("border","1px solid orange");
+	$(".icons").not("#icon3").css("border","1px solid transparent");
+};
+function autoIconLoop4(){
+	$(".claContent").html("");
+	$(".sun").html("");
+	$(".mon").html("");
+	$(".tue").html("未來商務展<br>");
+	$(".wed").html("未來商務展<br>");
+	$(".thu").html("");
+	$(".fri").html("");
+	$(".sat").html("隕石展<br>");
+	$(".claContent:lt(15)").append("成果展");
+	$("#icon4").css("border","1px solid orange");
+	$(".icons").not("#icon4").css("border","1px solid transparent");
+};
+
+// 手動點選月曆模式時，停掉輪播
+document.getElementById("icon1").onclick = manualIconShift1;
+document.getElementById("icon2").onclick = manualIconShift2;
+document.getElementById("icon3").onclick = manualIconShift3;
+document.getElementById("icon4").onclick = manualIconShift4;
+
+function manualIconShift1(){
+	$(".claContent").html("");
+	$(".sun").html("尋找星生命");
+	$(".mon").html("");
+	$(".tue").html("末世決戰");
+	$(".wed").html("尋找星生命");
+	$(".thu").html("");
+	$(".fri").html("尋找星生命");
+	$(".sat").html("末世決戰");
+	$("#icon1").css("border","1px solid orange");
+	$(".icons").not("#icon1").css("border","1px solid transparent");
+	stopShiftMode();
+};
+function manualIconShift2(){
+	$(".claContent").html("");
+	$(".claContent:eq(5)").append("設施1");
+	$(".claContent:eq(10)").append("設施2");
+	$(".claContent:eq(15)").append("設施3");
+	$(".claContent:eq(20)").append("設施4");
+	$(".claContent:eq(25)").append("設施5");
+	$(".claContent:eq(30)").append("設施6");
+	$("#icon2").css("border","1px solid orange");
+	$(".icons").not("#icon2").css("border","1px solid transparent");
+	stopShiftMode();
+};
+function manualIconShift3(){
+	$(".claContent").html("");
+	$(".sun").html("9-22");
+	$(".mon").html("休園");
+	$(".tue").html("10-22");
+	$(".wed").html("10-22");
+	$(".thu").html("10-22");
+	$(".fri").html("10-24");
+	$(".sat").html("9-24");
+	$("#icon3").css("border","1px solid orange");
+	$(".icons").not("#icon3").css("border","1px solid transparent");
+	stopShiftMode();
+};
+function manualIconShift4(){
+	$(".claContent").html("");
+	$(".sun").html("");
+	$(".mon").html("");
+	$(".tue").html("未來商務展<br>");
+	$(".wed").html("未來商務展<br>");
+	$(".thu").html("");
+	$(".fri").html("");
+	$(".sat").html("隕石展<br>");
+	$(".claContent:lt(15)").append("成果展");
+	$("#icon4").css("border","1px solid orange");
+	$(".icons").not("#icon4").css("border","1px solid transparent");
+	stopShiftMode();
+};
+function stopShiftMode(){
+	clearInterval(stop1);
+	clearInterval(stop2);
+	clearInterval(stop3);
+	clearInterval(stop4);
+};
 });
+
 //calendar---------------------------------------------------
 	//calendar---------------------------------------------------
 		$(window).on('load', function() {ajaxCallJsonP("http://api.openweathermap.org/data/2.5/forecast?","6696918");});
