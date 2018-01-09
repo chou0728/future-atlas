9049cbc609fb6650cd072024de16995f3be30c05
Manna Chen <sbxew99@gmail.com>
2018-01-09 18:21:06 +0800

    1.購物車更新 2.新增connect.php

--- js/cart_detail.js
+++ js/cart_detail.js
@@ -1,8 +1,7 @@
 var storage = localStorage;
-var today = new Date();
-// var todayYear = today.getFullYear();
-var expire_date = today.setDate(373);
-console.log(expire_date);
+var d = new Date();
+d.setFullYear(2019);
+var e = d.toString().substr(4,11).replace(" ","-")
 var member_id = "Sara168";
 var member_email = "Sara168@gmail.com";
 var member_points = 50;
@@ -18,13 +17,13 @@ window.onload = function(){
 			if( full_fare_num > 0 ){
 				var full_fare = info[0];
 				var full_fare_subtotal = full_fare * full_fare_num;			
-				$("#ticket_row").after("<tr><td>"+fn+"</td><td>"+"設施icon"+"</td><td>"+"設施名稱"+"</td><td>"+"全票"+"</td><td>"+expire_date+"</td><td>"+full_fare+"</td><td>"+full_fare_num+"</td><td class='sub_total'>"+full_fare_subtotal+"</td></tr>");
+				$("#ticket_row").after("<tr><td>"+fn+"</td><td>"+"設施icon"+"</td><td>"+"設施名稱"+"</td><td>"+"全票"+"</td><td>"+full_fare+"</td><td>"+full_fare_num+"</td><td class='sub_total' colspan='2'>"+full_fare_subtotal+"</td></tr>");
 			}
 			var half_fare_num = info[3];
 			if( half_fare_num > 0){
 				var half_fare = info[2];
 				var half_fare_subtotal = half_fare * half_fare_num;
-				$("#ticket_row").after("<tr><td>"+fn+"</td><td>"+"設施icon"+"</td><td>"+"設施名稱"+"</td><td>"+"半票"+"</td><td>"+expire_date+"</td><td>"+half_fare+"</td><td>"+half_fare_num+"</td><td class='sub_total'>"+half_fare_subtotal+"</td></tr>");
+				$("#ticket_row").after("<tr><td>"+fn+"</td><td>"+"設施icon"+"</td><td>"+"設施名稱"+"</td><td>"+"半票"+"</td><td>"+half_fare+"</td><td>"+half_fare_num+"</td><td class='sub_total' colspan='2'>"+half_fare_subtotal+"</td></tr>");
 			}
 		}
 	}
@@ -38,7 +37,6 @@ window.onload = function(){
 		$("#input_discount").before("<tr id='cart_total_row'><td colspan='7'>"+"總計："+"</td><td name='cart_sub_total' id='cart_sub_total'>"+total+"</td></tr>");	
 		document.getElementById("cart_sub_total").innerHTML = total;
 		document.getElementById("total").innerHTML = total;
-
 
 var points_confirm = document.getElementById("points_confirm");
 points_confirm.addEventListener("click",output_total);
@@ -68,28 +66,28 @@ function output_total(){
 	}else{
 		alert("您還沒有輸入積分唷!");
 		document.getElementsByClassName("points")[1].focus();
-		document.getElementsByClassName("points")[1].style.backgroundColor = "yellow";
+		document.getElementsByClassName("points")[1].style.backgroundColor = "navy";
 	}
 }
 
 // 使用者輸入積點
-document.getElementsByClassName("points")[1].onfocus = function(){
-	document.getElementsByClassName("points")[1].style.backgroundColor = "yellow";
+document.getElementsByClassName("points")[2].onfocus = function(){
+	document.getElementsByClassName("points")[2].style.backgroundColor = "navy";
 	$("#points_remain").css("opacity","1");
 }
 
 // 使用者勾選使用積點
-document.getElementsByClassName("points")[0].onchange = function(){
-	document.getElementsByClassName("points")[1].focus();
-	document.getElementsByClassName("points")[1].style.backgroundColor = "yellow";
+document.getElementsByClassName("points")[1].onchange = function(){
+	document.getElementsByClassName("points")[2].focus();
+	document.getElementsByClassName("points")[2].style.backgroundColor = "navy";
 }
 // 使用者勾選不使用積點
-document.getElementsByClassName("points")[2].onchange = function(){
+document.getElementsByClassName("points")[0].onchange = function(){
 	discount = 0;
 	document.getElementById("discount").innerHTML = discount;
-	document.getElementsByClassName("points")[1].value = "";
-	document.getElementsByClassName("points")[1].blur();
-	document.getElementsByClassName("points")[1].style.backgroundColor = "transparent";
+	document.getElementsByClassName("points")[2].value = "";
+	document.getElementsByClassName("points")[2].blur();
+	document.getElementsByClassName("points")[2].style.backgroundColor = "transparent";
 	document.getElementById("total").innerHTML = initial_total;
 	$("#points_remain_input").html(discount);
 	$("#points_remain").css("opacity","0");
