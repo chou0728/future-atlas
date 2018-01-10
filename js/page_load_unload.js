a23381dd84eb85b172b5c332305eb4b8dbbfe5f5
ISNARITA <takeshi5027@gmail.com>
2018-01-10 22:29:07 +0800

    頁面fadein/fadeout測試

---
+++ js/page_load_unload.js
@@ -0,0 +1,18 @@
+$(window).on('load', function(){
+  $('body').removeClass('fadeout');
+});
+
+$(function() {
+  // ハッシュリンク(#)と別ウィンドウでページを開く場合はスルー
+  $('a:not([href^="#"]):not([target])').on('click', function(e){
+    e.preventDefault(); // ナビゲートをキャンセル
+    url = $(this).attr('href'); // 遷移先のURLを取得
+    if (url !== '') {
+      $('body').addClass('fadeout');  // bodyに class="fadeout"を挿入
+      setTimeout(function(){
+        window.location = url;  // 0.8秒後に取得したURLに遷移
+      }, 400);
+    }
+    return false;
+  });
+});
