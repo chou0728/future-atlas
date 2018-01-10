ac0a69ab8069cc34b6f7dd960e3dcb0b905ead97
ISNARITA <takeshi5027@gmail.com>
2018-01-10 22:37:02 +0800

    修改fadeinfadeout程式

--- js/page_load_unload.js
+++ js/page_load_unload.js
@@ -4,7 +4,7 @@ $(window).on('load', function(){
 
 $(function() {
   // ハッシュリンク(#)と別ウィンドウでページを開く場合はスルー
-  $('a:not([href^="#"]):not([target])').on('click', function(e){
+  $('a:not([href^="#"]):not([target]):not([href^="javascript:void(0)"])').on('click', function(e){
     e.preventDefault(); // ナビゲートをキャンセル
     url = $(this).attr('href'); // 遷移先のURLを取得
     if (url !== '') {
