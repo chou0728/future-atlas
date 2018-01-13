7dc7fce6f30cafea91e380b13437779cf21a6745
Manna Chen <sbxew99@gmail.com>
2018-01-13 10:03:51 +0800

    註冊 表單驗證js

--- php/connectbd103g3.php
+++ php/connectbd103g3.php
@@ -1,5 +1,5 @@
 <?php
-$dsn = "mysql:host=140.115.236.72;port=3306;dbname=bd103g3;charset=utf8";
+$dsn = "mysql:host=localhost;port=3306;dbname=bd103g3;charset=utf8";
 $user = "bd103g3";
 $psw = "bd103g3";
 $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
