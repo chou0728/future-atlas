f4102ba4e5c6ddccf811120b7ffb4c7e2c691036
DavidChiaLee <david81229@gmail.com>
2018-01-16 13:12:30 +0800

    劇場場次清單可切換節目編號(未完成)、Theaterbuyticket改連結

---
+++ php/get_program_no.php
@@ -0,0 +1,44 @@
+<?php
+	try{
+		require_once("connectBooks.php");
+		$sql = "select * from theater_session_list where program_no=:programNo";
+		$theater_session_list = $pdo->prepare($sql);
+		$theater_session_list->bindValue(":programNo",$_REQUEST["programNo"]);
+		$theater_session_list->execute();
+
+	  if( $theater_session_list->rowCount() == 0 ){ //找不到
+	    //傳回空的字串
+	    echo "查無此節目";
+	  }else{ //找得到
+	    //取回一筆資料
+      $str = "";
+	  $str .="<table>";
+	  $str .="<tr>
+                    <th>場次編號</th>
+                    <th>節目編號</th>
+                    <th>場次時間</th>
+                    <th>演出日期</th>
+                    <th>總票數</th>
+                    <th>剩餘票數</th>
+                    <th>修改</th>
+              </tr>";	    
+	    while( $sessionlistRow = $theater_session_list->fetchObject()){
+	      //傳回html結構
+	      $str .= '<form method="get" action="update_theater_session_List.php" align="center" >';
+		  $str .= "<tr><td>" . $sessionlistRow->session_no . "</td>";
+		  $str .= "<td>" . $sessionlistRow->program_no . "</td>";
+		  $str .= "<td>" . $sessionlistRow->session_time . "</td>";
+		  $str .= "<td>" . $sessionlistRow->time_date . "</td>";
+		  $str .= "<td>" . '<input type="number" style="width:50px;" value="'. $sessionlistRow->total_ticket . '"" name="total_ticket">' . "</td>";
+		  $str .= "<td>" . '<input type="number" style="width:50px;" value="' . $sessionlistRow->last_ticket .  '"" name="last_ticket"> ' . "</td>";
+		  $str .= "<td>" . '<input type="submit" style="font-family:微軟正黑體;" value="修改">'. "</td></tr>";
+		  $str .= "</form>";
+	    }
+		  $str .=  "</table>";
+		  echo $str;	    
+	  } 
+
+	}catch(PDOException $e){
+	  echo $e->getMessage();
+	}
+?>
