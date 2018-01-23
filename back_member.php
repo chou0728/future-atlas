5585f9a85e2345d72eed5b85f4c7a5080237dff0
DavidChiaLee <david81229@gmail.com>
2018-01-23 11:45:39 +0800

    註解跳轉login

--- back_member.php
+++ back_member.php
@@ -3,10 +3,10 @@
 ob_start();
 session_start();
 //請複製:當無登入時會自動跳轉至登入頁面
-if(isset($_SESSION["login_success"])==false){
-    header("location:manager_login.php");
-    exit;
-}
+// if(isset($_SESSION["login_success"])==false){
+//     header("location:manager_login.php");
+//     exit;
+// }
 ?>
 
 <!-- ===========1 end=========== -->
